a84b53ec085d38566b984298942cf456f7a01bda
Ray Richardson <ray@simularity.com>
2024-07-14 10:16:14 -0400

    Added LRU Caches

--- adamantine/__init__.py
+++ adamantine/__init__.py
@@ -5,7 +5,9 @@ from adamantine.time_exec import time_exec
 from adamantine.apply import apply, apply_iter
 from adamantine.partial import papply 
 from adamantine.predicates import include, exclude, split, complement, all_of, some_of
+from adamantine.lru_cache import cached, LRUCache
 __all__ = ['tail_recursive', 'recurse', 'mutual', 'map', 'cmap', 'emap', 'foldl', 'groupby', 'group_count', 
            'groupby_set', 'merge', 'empty', 'time_exec', 'apply', 'apply_iter', 'papply', 'include',
            'exclude', 'split', 'complement', 'all_of', 'some_of', 'pairwise', 'pairwise_chain',
-           'map_iter', 'cmap_iter', 'emap_iter', 'pairwise_iter', 'pairwise_chain_iter'] 
+           'map_iter', 'cmap_iter', 'emap_iter', 'pairwise_iter', 'pairwise_chain_iter', 'cached', 'LRUCache']
+ 
---
+++ adamantine/lru_cache.py
@@ -0,0 +1,158 @@
+import pyrsistent as pyr
+from threading import Lock
+
+
+class _cache_node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+    def __repr__(self):
+        return f"({_cache_node.__name__} {self.key} {self.value})"
+
+    def __str__(self):
+        return f"({_cache_node.__name__} {self.key} {self.value})"
+
+class _cache_list:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.map = pyr.pmap()  # map from key to node
+
+    def append(self, node):
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        # add node to map
+        
+        return node
+
+    def unlink(self, node):
+        if node.prev is not None:
+            node.prev.next = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        if self.head == node:
+            self.head = node.next
+        if self.tail == node:
+            self.tail = node.prev
+        node.next = None
+        node.prev = None
+        return node
+
+    def move_to_end(self, node):
+        self.unlink(node)
+        return self.append(node)
+
+    def peek(self):
+        node = self.head
+        return node
+    
+    def remove(self, node):
+        self.unlink(node) 
+        self.map = self.map.remove(node.key)
+        return node
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+
+# Class LRUCache is an efficient LRU cache implementation. It takes a parameter size which determines the
+# capacity of the cache. If the size is None, the cache will have an infinite capacity. 
+# The cache is implemented using a persistent dictionary and a persistent queue.
+
+class LRUCache:
+    def __init__(self, size=None):
+        self.size = size
+        self.cache = pyr.pmap()
+        self.lru = _cache_list()
+        self.lock = Lock()
+        if size == None:
+            self.put = self.unbound_put
+            self.get = self.unbound_get
+        else:
+            self.put = self.bound_put
+            self.get = self.bound_get
+            
+    
+    # This method returns the value associated with the key in the cache.
+    def bound_get(self, key):
+        with self.lock:
+            # get node from self.lru.map
+            node = self.lru.map.get(key)
+            if node is not None:
+                value = self.cache[key]
+                self.lru.move_to_end(node)
+                return value
+        return None
+
+    def unbound_get(self, key):
+        with self.lock:
+            return self.cache.get(key)
+        
+    # This method puts the key-value pair in the cache. If the cache is full, it removes the least recently used
+    # key-value pair from the cache.
+    def bound_put(self, key, value):
+        with self.lock:
+            if len(self.cache) >= self.size:
+                # Get the oldest node with a pop()
+                lru_key_node = self.lru.peek()
+                lru_key = lru_key_node.key
+                # Remove the oldest key from the cache
+                self.cache = self.cache.remove(lru_key)
+                # Remove the oldest key from the LRU Index
+                self.lru.remove(lru_key_node)
+            self.cache = self.cache.set(key, value)
+            self.lru.append(key)
+            
+    def unbound_put(self, key, value):
+        with self.lock:
+            self.cache = self.cache.set(key, value)
+            self.lru = self.lru.append(key)
+            
+    # This method clears the cache.
+    def clear(self):
+        with self.lock:
+            self.cache = pyr.pmap()
+            self.lru = _cache_list()
+        
+
+# The function arg_key takes the args and kwargs of a function and returns a key that can be used to identify
+# the function call. This key is used to store the result of the function call in the cache.
+def arg_key(args, kwargs):
+    return (tuple(args), tuple(sorted(kwargs.items())))
+
+# the function lru_cache takes a size parameter which determines the capacity of the cache. It returns a decorator
+# that can be used to cache the results of a function call.
+def cached(max_size=None):
+    cache = LRUCache(max_size)    
+    def decorator(func):
+        
+        def wrapper(*args, **kwargs):
+            key = arg_key(args, kwargs)
+            value = cache.get(key)
+            if value is None:
+                value = func(*args, **kwargs)
+                cache.put(key, value)
+            return value
+        wrapper._lru_cache = cache
+        return wrapper
+    return decorator
+
+def clear_cache(wrapper):
+    if hasattr(wrapper, "_lru_cache"):
+        lru = wrapper._lru_cache
+        if isinstance(lru, LRUCache):
+            lru.clear()
+    return wrapper
+
+
